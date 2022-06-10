35014b9e28208d9d3ff48844a54d6f8d6164281e
Johnathan R. Burgess <ryanburgess173@outlook.com>
2022-06-10 10:16:58 -0400

    more changes

---
+++ RacingCultureWebScraper.BAL/Scrapers/SeasonStats/Base_Scraper.cs
@@ -0,0 +1,60 @@
+﻿using HtmlAgilityPack;
+using RacingCultureWebScraper.BAL.Models.SeasonStats;
+using System.Configuration;
+using System.Net;
+
+namespace RacingCultureWebScraper.BAL.Scrapers.SeasonStats
+{
+    internal class Base_Scraper
+    {
+        public SeasonStatsBase seasonStats { get; set; }
+        private string urlBase { get; set; }
+        private int yearToScrape { get; set; }
+        private char divisionToScrape { get; set; }
+        private string SeasonStatsUrlModifier { get; set; }
+        public HtmlDocument loadedHtmlDocument { get; set; }
+
+        public Base_Scraper(int year, char divisionModifier)
+        {
+            this.SeasonStatsUrlModifier = "/season-stats";
+            this.seasonStats = new SeasonStatsBase();
+            this.urlBase = ConfigurationManager.AppSettings["scrapingUrlBase"] ?? "";
+            this.yearToScrape = year;
+            this.divisionToScrape = divisionModifier;
+            string requestUrl = GenerateRequestUrl();
+            string responseString = GETRequest(requestUrl).Result;
+            this.loadedHtmlDocument = ParseResponse(responseString);
+        }
+
+        private HtmlDocument ParseResponse(string response)
+        {
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(response);
+            return htmlDocument;
+        }
+
+        private string GenerateRequestUrl()
+        {
+            return this.urlBase 
+                + this.SeasonStatsUrlModifier 
+                + "/" + this.yearToScrape.ToString() 
+                + "/" + this.divisionToScrape.ToString() + "/";
+        }
+
+        private async Task<string> GETRequest(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            string response = await client.GetStringAsync(fullUrl);
+            return response;
+        }
+
+        private Exception InvalidUrlException(string requestUrl)
+        {
+            Console.WriteLine("Url Invalid.");
+            Console.WriteLine(requestUrl);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RacingCultureWebScraper.BAL/Scrapers/SeasonStats/NASCAR/NASCAR_Scraper.cs
@@ -0,0 +1,16 @@
+﻿using RacingCultureWebScraper.BAL.Models.SeasonStats.NASCAR;
+
+namespace RacingCultureWebScraper.BAL.Scrapers.SeasonStats.NASCAR
+{
+    internal class NASCAR_Scraper : Base_Scraper
+    {
+        public NASCAR_Scraper(int year) : base(year)
+        {
+        }
+
+        private override SeasonStatsNASCAR ParseHtml()
+        {
+
+        }
+    }
+}
